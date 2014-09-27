369ce5c468711bb46b05d2b177f45f8ad31216d1
edkriss <Edward@Edwards-MacBook-Pro.local>
2014-09-27 15:01:44 +0800

    New project setup

---
+++ Business Order Tracking/Business Order Tracking/Document.h
@@ -0,0 +1,12 @@
+//
+//  Document.h
+//  Business Order Tracking
+//
+//  Created by Edward Christopher D'Silva on 9/27/14.
+//  Copyright (c) 2014 D Silva Trading. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSPersistentDocument
+@end
---
+++ Business Order Tracking/Business Order Tracking/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Business Order Tracking
+//
+//  Created by Edward Christopher D'Silva on 9/27/14.
+//  Copyright (c) 2014 D Silva Trading. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
