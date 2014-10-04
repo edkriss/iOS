95af237a604de67f3716d9bac269e678e697b796
edkriss <Edward@Edwards-MacBook-Pro.local>
2014-10-04 18:50:04 +0800

    20141004

---
+++ Business Tracking iPhone/Business Tracking iPhone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Business Tracking iPhone
+//
+//  Created by Edward Christopher D'Silva on 9/29/14.
+//  Copyright (c) 2014 D Silva Trading. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Business Tracking iPhone/Business Tracking iPhone/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Business Tracking iPhone
+//
+//  Created by Edward Christopher D'Silva on 9/29/14.
+//  Copyright (c) 2014 D Silva Trading. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
